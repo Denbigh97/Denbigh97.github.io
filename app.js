3f98c6e20f45f5a33a79bfe1bd242c34e2515399
bruce muckerson <denby79@yahoo.com>
2020-12-08 21:54:44 -0600

    up
    up

--- app.js
+++ app.js
@@ -7,27 +7,27 @@ let trace1  = {
     x: data.map(d => d.TEAM_ABBREVIATION),
     y: data.map(d => d.PTS),
     name: 'avg pts year'
-}
+};
 
 let trace2  = {
     x: data.map(d => d.TEAM_ABBREVIATION),
     y: data.map(d => d.SEASON_ID),
     name: 'teams'
-}   
+};
 
 let trace3  = {
     x: data.map(d => d.TEAM_ABBREVIATION),
     y: data.map(d => d.Is_Win),
     name: 'wins'
-}
+};
 
 let plotData = [trace1, trace2, trace3]
 
 let plotLayout = {
     title: "NBA STATS"
-}
+};
   
   
-Plotly.newPlot('myPlot', plotData)
+Plotly.newPlot('myPlot', plotData);
 
 
